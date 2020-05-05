9d036e8d0763bdf11e15686692976f66a7875195
wwsalmon <wwsamson@yahoo.com>
2020-05-05 19:59:08 -0400

    add home hero image

--- inc/customizer.php
+++ inc/customizer.php
@@ -4,15 +4,11 @@ function s4f_customizer($wp_customize)
     $wp_customize->add_section('s4f-section', array(
         'title' => "Theme Custom Settings"
     ));
-    $wp_customize->add_setting('s4f-image');
-    $wp_customize->add_control(new WP_Customize_Cropped_Image_Control($wp_customize, 's4f-image-control', array(
+    $wp_customize->add_setting('s4f-home-image');
+    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 's4f-home-image-control', array(
         "label" => "image field",
         "section" => "s4f-section",
-        "settings" => "s4f-image",
-        "width" => 900,
-        "height" => 600,
-        "flex-width" => true,
-        "flex-height" => true
+        "settings" => "s4f-home-image"
     )));
     $wp_customize->add_setting('s4f-text');
     $wp_customize->add_control('s4f-text-control', array(
--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php get_header();
 ?>
+<div class="home-hero" style="background-image: url('<?php echo get_theme_mod('s4f-home-image'); ?>');"></div>
+<div class="home-top container">
+
+</div>
+<div class="home-section container">
+    <h1>Our Impact</h1>
+</div>
     <div class="container">
         <?php
         if (have_posts()):
