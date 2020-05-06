dc086bcc6a1924cff1d9aa24e9ba2a01d1b3a046
wwsalmon <wwsamson@yahoo.com>
2020-05-05 22:49:47 -0400

    fix navbar zindex problem, add blog back button

--- single.php
+++ single.php
@@ -1,10 +1,11 @@
 <?php get_header();
 ?>
     <div class="container content-container single-container">
+        <div class="blog-back"><a href="">< Back to All Posts</a></div>
         <?php
         while (have_posts()) : the_post(); ?>
             <h1><?php the_title(); ?></h1>
-            <div class="single-date"><?php echo the_date("F d, Y"); ?></div>
+            <div class="single-date"><?php echo the_date("F n, Y"); ?></div>
             <div class="single-author"><span><?php echo sz_author_with_link(get_the_author_meta("ID")); ?></span></div>
             <?php
             the_content();
