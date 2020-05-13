3470f4cee04b533d008d8fc938beee8d10d240f4
wwsalmon <wwsamson@yahoo.com>
2020-05-13 19:42:03 -0400

    close #4, make blog posts much prettier

--- single.php
+++ single.php
@@ -1,16 +1,20 @@
 <?php get_header();
 ?>
-    <div class="container content-container single-container">
-        <div class="blog-back"><a href="<?php echo get_site_url()?>/blog">< Back to All Posts</a></div>
+    <div class="blog-top <?php if (catch_that_image(get_the_ID()) != false) echo "post-with-image" ?>">
+        <div class="blog-top-inner">
+            <div class="blog-back"><a href="<?php echo get_site_url() ?>/blog">< Back to All Posts</a></div>
         <?php
         while (have_posts()) : the_post(); ?>
             <h1><?php the_title(); ?></h1>
             <div class="single-date"><?php echo the_date("F n, Y"); ?></div>
             <div class="single-author"><span><?php echo sz_author_with_link(get_the_author_meta("ID")); ?></span></div>
-            <?php
-            the_content();
-        endwhile;
-        ?>
+        </div>
+    </div>
+    <div class="container content-container single-container <?php if (catch_that_image(get_the_ID()) != false) echo "post-with-image" ?>">
+    <?php
+    the_content();
+endwhile;
+?>
     </div>
 <?php
 get_footer();
