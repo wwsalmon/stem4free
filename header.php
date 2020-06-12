0f61bebf2f0544c265f331552296e3136e156119
Samson Zhang <wwsamson@yahoo.com>
2020-06-12 12:02:47 -0400

    add utility classes and apply to navbar, start of home

--- header.php
+++ header.php
@@ -13,21 +13,21 @@ wp_head();
 </head>
 <body>
 <div class="sz-navbar s4f-navbar">
-    <div class="sz-navbar-inner sz-navbar-right">
+    <div class="sz-navbar-inner sz-navbar-right container-1200">
         <div class="navbar-logo">
             <a href="<?php echo get_home_url() ?>">
                 <img src="<?php echo get_template_directory_uri() ?>/img/logo-small.png">
             </a>
             <a href="<?php echo get_home_url() ?>">
-                <span><?php echo get_bloginfo("name") ?></span>
+                <span class="font-bold font-24 ml-3"><?php echo get_bloginfo("name") ?></span>
             </a>
         </div>
         <input type="checkbox" id="sz-navbar-check">
         <label for="sz-navbar-check" class="sz-navbar-hamburger">☰</label>
-        <div class="sz-navbar-items">
+        <div class="sz-navbar-items font-mono font-bold font-16">
             <?php
             foreach (get_menu_items_by_registered_slug("primary") as $item){
-                echo "<div class=\"sz-navbar-item\"><span><a href=\"" . $item->url . "\">" . $item->title . "</a></span></div>";
+                echo "<div class=\"sz-navbar-item ml-3\"><span><a href=\"" . $item->url . "\">" . $item->title . "</a></span></div>";
             }
             ?>
         </div>
--- page-home.php
+++ page-home.php
@@ -1,4 +1,9 @@
 <?php get_header();
 ?>
+<div class="container-1200 mt-6">
+    <p class="font-l-responsive width-700 text-center">Stem4Free is a nonprofit corporation dedicated to food rescue and food waste awareness.</p>
+    <h1 class="font-xl-responsive text-center">8,243 meals rescued</h1>
+    <p class="width-700 text-center font-mono font-16-lt-600 font-bold"><span class="opacity-20">in Massachusetts (1,240), Hong Kong (942), and 9 other locations,</span><span class="opacity-60">with 1,234 meals rescued in the last week.</span></p>
+</div>
 <?php
 get_footer();
