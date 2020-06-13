3edd5954019e1b5cc5d1e6aeed2e4e2ff01b8b85
Samson Zhang <wwsamson@yahoo.com>
2020-06-13 00:02:12 -0400

    start implementing blog/news formatting

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php get_header();
 ?>
+	<div class="width-700 container mt-6 pt-6 pb-6 page-container">
+		<?php
+		if (have_posts()):
+			while (have_posts()): the_post();
+				the_content();
+			endwhile;
+		endif;
+		?>
+	</div>
 <?php
 get_footer();
--- single.php
+++ single.php
@@ -1,4 +1,18 @@
 <?php get_header();
 ?>
+	<div class="width-700 container mt-6 pt-6 pb-6 page-container">
+        <div class="font-mono opacity-20"><a href="<?php echo get_home_url() ?>/blog" class="font-no-underline">&lt; All Posts</a></div>
+		<?php
+		if (have_posts()):
+			while (have_posts()): the_post();
+				echo "<h1 class='font-l-responsive'>" . get_the_title() . "</h1>";
+				echo "<div class='font-mono opacity-40'><span><a href='" . get_author_posts_url(get_the_author_meta("id")) . "'>" . get_the_author() . "</a></span></div>";
+				echo "<div class='font-mono opacity-20'><span>" . get_the_time("M j, Y") . "</span></div>";
+				echo "<hr class='mt-3 mb-3 opacity-20'/>";
+				the_content();
+			endwhile;
+		endif;
+		?>
+	</div>
 <?php
 get_footer();
