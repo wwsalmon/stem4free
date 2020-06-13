d8459f31e3c4a4a9aef3625919fb3e747388e8df
Samson Zhang <wwsamson@yahoo.com>
2020-06-13 08:37:59 -0400

    fix blog header showing up on static pages

--- page.php
+++ page.php
@@ -1,7 +1,6 @@
 <?php get_header();
 ?>
     <div class="width-700 container mt-6 pt-6 pb-6 page-container">
-        <h1 class="font-l-responsive text-center">The Latest from Stem4Free</h1>
 		<?php
 		if (have_posts()):
 			while (have_posts()): the_post();
