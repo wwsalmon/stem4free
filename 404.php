0a3893afdbc727fd3a669712d469bad3a01810b4
Samson Zhang <wwsamson@yahoo.com>
2020-06-17 17:28:06 -0400

    close #15, add 404 page

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php
+get_header();
+?>
+<div class="width-700 pt-6 pb-6 page-container content-container">
+    <h1>404 error: No page at this url</h1>
+    <p>You might have followed a broken link. Double check that everything in the url is spelled correctly, or <a href="<?php echo get_home_url(); ?>">return home.</a></p>
+</div>
+<?php
+get_footer();
+?>
